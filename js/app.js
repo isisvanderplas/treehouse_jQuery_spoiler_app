c97106acd670de4b90faafece90487fcf76ff097
Isis van der Plas <isisvanderplas@hotmail.com>
2018-01-09 04:15:12 +0100

    add functionality to reveal-button

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
-
-
-
-
-
+// hide the spoiler text
+$('.spoiler span').hide();
+// when the button is pressed:
+$('.spoiler button').click(() => {
+  $('.spoiler span').show();
+  $('.spoiler button').hide()
+})
+  // show the spoiler text
+  // hide the 'reveal spoiler' button
